34d43831393619a28282e25680a032b9fd701067
gjel64 <mattinnoblia64@gmail.com>
2026-05-21 16:48:44 +0200

    add a test for indexing

--- tests/tensor_test.cpp
+++ tests/tensor_test.cpp
@@ -101,12 +101,32 @@ TEST(TensorTest, Indexing)
     EXPECT_EQ( ((*(*t2)[1])[0, 1]->item()), 6.0f );
     EXPECT_EQ( ((*(*t2)[1])[1, 1]->item()), 8.0f );
     EXPECT_EQ( ((*(*t2)[0])[1, 1]->item()), 4.0f );
+    EXPECT_EQ( (*(*(*t2)[0])[1])[1]->item(), 4.0f );
 
 }
 
 TEST (TensorTest, Addition)
 {
-    
+    /*std::shared_ptr<Tensor> t1 = std::make_shared<Tensor>(std::vector<std::vector<float>>{{1.0f, 2.0f}, {3.0f, 4.0f}});
+    std::shared_ptr<Tensor> t2 = std::make_shared<Tensor>(std::vector<std::vector<float>>{{5.0f, 6.0f}, {7.0f, 8.0f}});
+    std::shared_ptr<Tensor> t3 = (*t1) + t2;
+    EXPECT_EQ(t3->shape(), (std::vector<std::size_t>{2, 2}));
+    EXPECT_FLOAT_EQ( ((*t3)[0, 0]->item()), 6.0f );
+    EXPECT_FLOAT_EQ( ((*t3)[1, 1]->item()), 12.0f );
+
+    std::shared_ptr<Tensor> t4 = std::make_shared<Tensor>(std::vector<float>{7.0f, 8.0f});
+    std::shared_ptr<Tensor> t5 = (*t1) + t4;
+    EXPECT_EQ(t5->shape(), (std::vector<std::size_t>{2, 2}));
+    EXPECT_FLOAT_EQ( ((*t5)[0, 0]->item()), 8.0f );
+    EXPECT_FLOAT_EQ( ((*t5)[1, 1]->item()), 12.0f );
+
+    std::shared_ptr<Tensor> t6 = std::make_shared<Tensor>(10.0f);
+    std::shared_ptr<Tensor> t7 = (*t1) + t6;
+    EXPECT_EQ(t7->shape(), (std::vector<std::size_t>{2, 2}));
+    EXPECT_FLOAT_EQ( ((*t7)[0, 0]->item()), 11.0f );
+    EXPECT_FLOAT_EQ( ((*t7)[1, 1]->item()), 14.0f );*/
+
+
 }
 
 
