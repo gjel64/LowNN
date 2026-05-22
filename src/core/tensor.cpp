cc73440c3cdd7296b3a053972c8946843007d375
gjel64 <mattinnoblia64@gmail.com>
2026-05-22 10:47:08 +0200

    add + operator on tensors

--- src/core/tensor.cpp
+++ src/core/tensor.cpp
@@ -1,6 +1,9 @@
 #include "core/tensor.hpp"
 
 
+
+// ------------------------ Init ------------------------
+
 Tensor::Tensor(std::shared_ptr<float[]> data, std::vector<std::size_t> shape, std::size_t offset, std::vector<std::size_t> strides) 
     : _pdata(data), _shape(shape), _offset(offset), _strides(strides) 
 {
@@ -10,6 +13,7 @@ Tensor::Tensor(std::shared_ptr<float[]> data, std::vector<std::size_t> shape, st
     }
     _size = size;
 }
+
 
 
 // ------------------------ Methods ------------------------
@@ -37,7 +41,78 @@ void Tensor::print()
 // ------------------------ Operators ------------------------ 
 
 std::shared_ptr<Tensor> Tensor::operator+ (std::shared_ptr<Tensor> other){
-        std::vector<std::size_t> result_shape = VecUtils::broadcast(this->shape(), other->shape());    
 
+        const std::shared_ptr<float[]> other_data = other->data();
+        const std::size_t other_offset = other->offset();
+        const std::vector<std::size_t> other_strides = other->strides();
+        const std::vector<std::size_t> other_shape = other->shape();
+
+        std::vector<std::size_t> result_shape = VecUtils::broadcast(this->shape(), other->shape());
+
+        // calc strides of result
+        std::vector<std::size_t> result_stride(result_shape.size(), 1);
+        if (result_shape.size() > 0) {
+            for (int i = result_shape.size() - 2; i >= 0; i--) {
+                result_stride[i] = result_shape[i + 1] * result_stride[i + 1];
+            }
+        }
+
+        // calc size of result
+        std::size_t result_size = 1;
+        for (std::size_t dim_size : result_shape){
+            result_size *= dim_size;
+        }
+
+        std::shared_ptr<float[]> result_data = std::make_shared<float[]>(result_size);
+
+        if (result_size > 0) {
+            // calc strides for this and other with broadcasting
+            std::vector<std::size_t> this_b_strides(result_shape.size(), 0);
+            std::vector<std::size_t> other_b_strides(result_shape.size(), 0);
+
+            int this_dim_diff = result_shape.size() - _shape.size();
+            int other_dim_diff = result_shape.size() - other_shape.size();
+
+            for (std::size_t dim = 0; dim < result_shape.size(); dim++) {
+                if (dim >= this_dim_diff) {
+                    std::size_t this_dim = dim - this_dim_diff;
+                    if (_shape[this_dim] != 1) {
+                        this_b_strides[dim] = _strides[this_dim];
+                    }
+                }
+
+                if (dim >= other_dim_diff) {
+                    std::size_t other_dim = dim - other_dim_diff;
+                    if (other_shape[other_dim] != 1) { 
+                        other_b_strides[dim] = other_strides[other_dim];
+                    }
+                }
+            }
+
+            std::vector<std::size_t> indices(result_shape.size(), 0);
+            std::size_t this_i = 0;
+            std::size_t other_i = 0;
+
+            // main loop to fill result data
+            for (std::size_t i = 0; i < result_size; i++) {
+                result_data[i] = _pdata[this_i + _offset] + other_data[other_i + other_offset];
+
+                for (int dim = result_shape.size() - 1; dim >= 0; dim--) {
+                    indices[dim]++;
+                    if (indices[dim] < result_shape[dim]) {
+                        this_i += this_b_strides[dim];
+                        other_i += other_b_strides[dim];
+                        break;
+                    } 
+                    else {
+                        indices[dim] = 0;
+                        this_i -= this_b_strides[dim] * (result_shape[dim] - 1);
+                        other_i -= other_b_strides[dim] * (result_shape[dim] - 1);
+                    }
+                }
+            }
+        }
+
+        return std::make_shared<Tensor>(result_data, result_shape, 0, result_stride);
         
 }
--- tests/tensor_test.cpp
+++ tests/tensor_test.cpp
@@ -107,25 +107,45 @@ TEST(TensorTest, Indexing)
 
 TEST (TensorTest, Addition)
 {
-    /*std::shared_ptr<Tensor> t1 = std::make_shared<Tensor>(std::vector<std::vector<float>>{{1.0f, 2.0f}, {3.0f, 4.0f}});
+
+    std::shared_ptr<Tensor> t1 = std::make_shared<Tensor>(std::vector<std::vector<float>>{{1.0f, 2.0f}, {3.0f, 4.0f}});
     std::shared_ptr<Tensor> t2 = std::make_shared<Tensor>(std::vector<std::vector<float>>{{5.0f, 6.0f}, {7.0f, 8.0f}});
     std::shared_ptr<Tensor> t3 = (*t1) + t2;
     EXPECT_EQ(t3->shape(), (std::vector<std::size_t>{2, 2}));
     EXPECT_FLOAT_EQ( ((*t3)[0, 0]->item()), 6.0f );
     EXPECT_FLOAT_EQ( ((*t3)[1, 1]->item()), 12.0f );
-
+    
+    
     std::shared_ptr<Tensor> t4 = std::make_shared<Tensor>(std::vector<float>{7.0f, 8.0f});
     std::shared_ptr<Tensor> t5 = (*t1) + t4;
     EXPECT_EQ(t5->shape(), (std::vector<std::size_t>{2, 2}));
     EXPECT_FLOAT_EQ( ((*t5)[0, 0]->item()), 8.0f );
     EXPECT_FLOAT_EQ( ((*t5)[1, 1]->item()), 12.0f );
-
+    
     std::shared_ptr<Tensor> t6 = std::make_shared<Tensor>(10.0f);
     std::shared_ptr<Tensor> t7 = (*t1) + t6;
     EXPECT_EQ(t7->shape(), (std::vector<std::size_t>{2, 2}));
     EXPECT_FLOAT_EQ( ((*t7)[0, 0]->item()), 11.0f );
-    EXPECT_FLOAT_EQ( ((*t7)[1, 1]->item()), 14.0f );*/
+    EXPECT_FLOAT_EQ( ((*t7)[1, 1]->item()), 14.0f );
+    
+    std::shared_ptr<Tensor> t8 = std::make_shared<Tensor>(std::vector<std::vector<float>>{{1.0f}, {2.0f}});
+    std::shared_ptr<Tensor> t9 = std::make_shared<Tensor>(std::vector<std::vector<float>>{{3.0f, 4.0f}});
+    std::shared_ptr<Tensor> t10 = (*t8) + t9;
+    EXPECT_EQ(t10->shape(), (std::vector<std::size_t>{2, 2}));
+    EXPECT_FLOAT_EQ( ((*t10)[0, 0]->item()), 4.0f );
+    EXPECT_FLOAT_EQ( ((*t10)[0, 1]->item()), 5.0f );
+    EXPECT_FLOAT_EQ( ((*t10)[1, 0]->item()), 5.0f ); 
+    EXPECT_FLOAT_EQ( ((*t10)[1, 1]->item()), 6.0f ); 
 
+    std::shared_ptr<Tensor> t11 = std::make_shared<Tensor>(std::vector<std::vector<std::vector<float>>>{
+        {{1.0f, 2.0f}, {3.0f, 4.0f}}, {{5.0f, 6.0f}, {7.0f, 8.0f}}
+    });
+    std::shared_ptr<Tensor> t12 = std::make_shared<Tensor>(std::vector<float>{10.0f, 20.0f});
+    std::shared_ptr<Tensor> t13 = (*t11) + t12;
+    EXPECT_EQ(t13->shape(), (std::vector<std::size_t>{2, 2, 2}));
+    EXPECT_FLOAT_EQ( ((*t13)[0, 0, 0]->item()), 11.0f );
+    EXPECT_FLOAT_EQ( ((*t13)[0, 0, 1]->item()), 22.0f );
+    EXPECT_FLOAT_EQ( ((*t13)[1, 1, 1]->item()), 28.0f );
 
 }
 
